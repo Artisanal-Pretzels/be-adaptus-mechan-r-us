ffa05c46dc71ea19c9c1a28b1b1d291f08890216
steferguson <52539323+steferguson@users.noreply.github.com>
2019-10-23 18:33:00 +0100

    Created DbContext

---
+++ BackEnd/Data/AmruDbContext.cs
@@ -0,0 +1,28 @@
+using BackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Data
+{
+    public class AmruDbContext : DbContext
+    {
+        public AmruDbContext (DbContextOptions<AmruDbContext> options) : base (options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<PhoneNumber> PhoneNumbers { get; set; }
+
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating (modelBuilder);
+
+            modelBuilder.Entity<User> ().ToTable ("Users");
+            modelBuilder.Entity<PhoneNumber> ().ToTable ("PhoneNumbers");
+
+            modelBuilder.Entity<User> (entity =>
+            {
+                entity.Property (e => e.CreatedAt).HasDefaultValueSql ("CURRENT_TIMESTAMP").ValueGeneratedOnAdd ();
+                entity.Property (e => e.LastUpdatedAt).HasDefaultValueSql ("CURRENT_TIMESTAMP").ValueGeneratedOnAddOrUpdate ();
+            });
+
+        }
+    }
+}
