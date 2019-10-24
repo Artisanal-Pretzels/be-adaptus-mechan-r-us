732b7c10469dfc17c5941bdb54ff3f1fdd9a3e89
steferguson <52539323+steferguson@users.noreply.github.com>
2019-10-24 23:42:56 +0100

    Login endpoint complete

---
+++ BackEnd/Models/UserDTO.cs
@@ -0,0 +1,16 @@
+namespace BackEnd.Models
+{
+  public class UserDTO
+  {
+    public uint UserID { get; set; }
+
+    public string Username { get; set; }
+
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+  }
+}
