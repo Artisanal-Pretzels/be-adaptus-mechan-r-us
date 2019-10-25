9cf6b0cd0bacde864b90ae6ce8c8ca5a87ae9a94
steferguson <52539323+steferguson@users.noreply.github.com>
2019-10-25 13:10:14 +0100

    create registration endpoint

---
+++ FrontEndRequests/UserRegistration.cs
@@ -0,0 +1,19 @@
+namespace FrontEndRequests
+{
+  public class UserRegistration
+  {
+
+    public string Username { get; set; }
+
+    public string Password { get; set; }
+
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string PaymentEmail { get; set; }
+
+  }
+}
