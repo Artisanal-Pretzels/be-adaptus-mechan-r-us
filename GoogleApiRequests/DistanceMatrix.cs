b2ac1d7ef79176881cdaa33aa4b204a22ce0cb18
smetca <s.metcalfe.h@gmail.com>
2019-10-25 11:46:16 +0100

    Create distance matrix class

--- GoogleApiRequests/DistanceMatrix.cs
+++ GoogleApiRequests/DistanceMatrix.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using GoogleApi.Entities.Common;
+using GoogleApi;
+using GoogleApi.Entities.Maps.DistanceMatrix.Request;
+using System.Threading.Tasks;
 
 namespace GoogleApiRequests
 {
-  public class DistanceMatrix
+  public static class DistanceMatrix
   {
-    public 
+    public static async Task<dynamic> GetGarageDistances(double longitude, double latitude, List<GarageDestinationLocation> garageLocations)
+    {
+
+      var request = new DistanceMatrixRequest
+        {
+          Key = ApiKey.apiKey,
+          Origins = new[] { new Location(latitude, longitude)},
+          Destinations = getLocations(garageLocations)
+        };
+
+      var response = await GoogleMaps.DistanceMatrix.QueryAsync(request);
+      return response;
+    }
+
+    public static Collection<Location> getLocations(List<GarageDestinationLocation> garageLocations)
+    {
+      Collection<Location> locations = new Collection<Location>();
+
+      foreach (var garage in garageLocations)
+      {
+        locations.Add(new Location(garage.Latitude, garage.Longitude));
+      }
+
+      return locations;
+    }
   }
 }
