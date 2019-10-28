6e0391aa9c5f488a6ff079dc5c11b296c1c51250
smetca <49376655+smetca@users.noreply.github.com>
2019-10-28 10:13:49 +0000

    Merge fix (#22)
    * Added tests folder
    
    * Add first Garage endpoint test

---
+++ tests/BackEndTests/GarageControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BackEndTests
+{
+    public class GarageControllerTests
+    {
+        [Fact]
+        public void GetGarageIDEndpoint_ValidID_ReturnsValidGarage()
+        {
+            
+        }
+    }
+}
